ae6c8ab058353dcff174c884fd7373d0ec3daf7d
AdrianHuerta07 <al05-029-0325@utdelacosta.edu.mx>
2026-07-29 23:53:01 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'piloncillos-flashcards-v29';
+const CACHE_NAME = 'piloncillos-flashcards-v30';
 
 // Recusos esenciales para el funcionamiento Offline de la App
 const ASSETS_TO_CACHE = [
